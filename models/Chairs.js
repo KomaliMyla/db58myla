f3e5557b12243979607a9e70fc0cc1b9f7e30bd1
KomaliMyla <89554583+KomaliMyla@users.noreply.github.com>
2021-11-27 21:45:32 -0600

    edited

--- models/Chairs.js
+++ models/Chairs.js
@@ -3,7 +3,7 @@ const { SchemaTypes } = require('mongoose');
 
 const ChairsSchema = mongoose.Schema({
 Chair_type: String,
-Color:  {String, enum: ['Black', 'Blue','Red','Green', 'Brown'], required: function() { return this.Color;}},
+Color: {type: String, enum: ['White', 'Red','Yellow','Pink', 'Black'], required: function() { return this.Color;}},
 Cost: {type: Number, min:[20], max:[2000] }})
 module.exports = mongoose.model("Chairs",ChairsSchema);
 

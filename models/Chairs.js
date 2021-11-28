fb3639beca84c12566d51b064735d7924346374c
KomaliMyla <89554583+KomaliMyla@users.noreply.github.com>
2021-11-27 20:38:37 -0600

    edited

--- models/Chairs.js
+++ models/Chairs.js
@@ -4,6 +4,6 @@ const { SchemaTypes } = require('mongoose');
 const ChairsSchema = mongoose.Schema({
 Chair_type: String,
 Color: String,
-Cost: {type: Number, min:[20], max:[200] }})
+Cost: {type: Number, min:[20], max:[2000] }})
 module.exports = mongoose.model("Chairs",ChairsSchema);
 
